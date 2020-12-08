34fc4f7963035be34d0cfe9326ebd11c4e212f20
fareska <fares.xb@gmail.com>
2020-12-08 22:44:09 +0200

    basic logic and api manager

--- dist/apiManager.js
+++ dist/apiManager.js
@@ -1,6 +1,24 @@
 class ApiManager{
     
-  
+    async creatNewUser(newUser){
+        const savedUser = await $.post('/user', newUser)
+        return (savedUser)
+    } 
+    
+    async signInUser(emailPassword){
+        const user = await $.post('/signIn', emailPassword)
+        return (user)
+    } 
+    
+    async getUserFromDB(userId){
+        const userData = await $.get(`user/${userId}`)
+        return (userData)
+    }
+
+    async updateUserStatus(userId, userWeight){
+        const updatedUser = await $.put(`/user?userId=${userId}&weight=${userWeight}`)
+        return (updatedUser)
+    } 
 }
 
 
