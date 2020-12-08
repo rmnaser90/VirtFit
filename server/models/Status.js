1891a7a7186b8a2573f234605a9ece477026312a
rmnaser90 <rmnaser90@gmail.com>
2020-12-08 15:06:55 +0200

    finish routes and schema, with db errorr

--- server/models/Status.js
+++ server/models/Status.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const StatusSchema = new Schema({
+    weight:	Number,
+    bmrValue: Number,	
+    status:	String,
+    risk:	String,
+    idealWeight: Number,
+    timeStamp:	Date,
+    trainingClories: Number
+})
 
-
-
-
-
-
+const Status = mongoose.model('status',StatusSchema)
 
 
 
--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,36 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const Status = require('./Status')
+const UserSchema = new Schema({
+    firstName:	String,
+    lastName:	String,
+    email:	String,
+    password:	String,
+    phoneNo:	String,
+    gender:	String,
+    birthdate:	Date,
+    height:	Number,
+    weight: Number
+    
+})
 
 
 
+const User = mongoose.model('user',UserSchema)
 
+const newUser =  new User({
+    firstName:	"rami",
+    lastName:	"rami",
+    email:	'rami@rami.com',
+    password:	'asdasd',
+    phoneNo:	"05232323",
+    gender:	'm',
+    birthdate:	Date.now(),
+    height:	170,
+    weight:72
+})
 
-
-
-
-
+console.log(newUser);
+newUser.save()
 
 module.exports = User
