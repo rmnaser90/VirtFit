c4a1afd0158c219c9521cbf6db73b7a542c4d3ca
HealthyJunkie <androwas101@gmail.com>
2020-12-08 16:50:36 +0200

    added features

--- dist/main.js
+++ dist/main.js
@@ -12,9 +12,20 @@ if (isLoggedIn()) {
     })
     $('#sign-in-sign-up').on('click', '#sign-in', function () {
         console.log("TBD sign in")
+        const email = $('#email').val()
+        const password = $('#password').val()
     })
     $('#sign-in-sign-up').on('click', '#sign-up', function () {
         console.log("TBD submit sign up")
+        const firstName = $('#firstName').val()
+        const lastName = $('#lastName').val()
+        const email = $('#email').val()
+        const phoneNumber = $('#phoneNumber').val()
+        const gender = $('#gender').val()
+        const birthDate = $('#birthDate').val()
+        const height = $('#height').val()
+        const weight = $('#weight').val()
+        const password = $('#password').val()
     })
     
 }
