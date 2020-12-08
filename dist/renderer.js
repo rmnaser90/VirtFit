837eaffbb6cbf8765d7313ebccd2cc26f3e14f5b
HealthyJunkie <androwas101@gmail.com>
2020-12-08 14:46:49 +0200

    first commit to index-render-initial

--- dist/renderer.js
+++ dist/renderer.js
@@ -1,4 +1,17 @@
 class Renderer {
+    constructor() {
+        const signInSource = $('#sign-in-template').html();
+        this.signInTemplate = Handlebars.compile(signInSource);
 
-  
+        const signUpSource = $('#sign-up-template').html();
+        this.signUpTemplate = Handlebars.compile(signUpSource);
+    }
+
+    _handleBarAppender = (Template) => {
+        let newHTML = Template();
+        $('#sign-in-sign-up').empty().append(newHTML);
+    }
+
+    renderSignIn = () => this._handleBarAppender(this.signInTemplate)
+    renderSignUp = () => this._handleBarAppender(this.signUpTemplate)
 }
