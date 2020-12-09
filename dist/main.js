9e2c3c8ffd10ee1c138b267f6c8e343b2c5b6a34
HealthyJunkie <androwas101@gmail.com>
2020-12-09 20:44:35 +0200

    some changes...

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,6 @@
 const virtFitApp = new VirtFitAPP
 const renderer = new Renderer
 
-
 if (isLoggedIn()) {
     renderer.renderSignIn()
 
@@ -16,30 +15,39 @@ if (isLoggedIn()) {
             email : $('#email').val(),
             password : $('#password').val()
         }
-        const user = await virtFitApp.signInUser(emailPass)
-        localStorage.id = user._id
-        location.assign(`./user/user.html`)
+        const signInFunction = getSignInFunction()
+
+        console.log(signInFunction)
+
+        const user = await signInFunction(emailPass)
+        if(user.error)
+            $('#err').empty().text(user.error)
+        else{
+            const path = getPath()
+
+            localStorage.id = user._id
+            location.assign(path)
+        }
     })
 
     $('#sign-in-sign-up').on('click', '#sign-up',async function () {
-        const user = {
-            firstName : $('#firstName').val(),
-            lastName : $('#lastName').val(),
-            email : $('#email').val(),
-            phoneNumber : $('#phoneNumber').val(),
-            gender : $('#gender').val(),
-            birthdate :new Date($('#birthDate').val()) ,
-            height : $('#height').val(),
-            weight : $('#weight').val(),
-            password : $('#password').val()
+        const user = extractSignUpInputs()
+        const missingInput = missingInputCheck(user)
+
+        if(missingInput)
+            $('#err').empty().text(`${missingInput} is missing`)
+        else{
+            const signUpFunction = getSignUpFunction()()
+            const path = getPath()
+
+            const newUser = await signUpFunction(user)
+            localStorage.id = newUser._id
+            location.assign(path)
         }
-        const newUser = await virtFitApp.creatNewUser(user)
-        
-        localStorage.id = newUser._id
-        location.assign(`./user/user.html`)
     })
     
 }
+//createNewTrainer
 
 
 function isLoggedIn() {
@@ -51,3 +59,32 @@ function isLoggedIn() {
     return true
 }
 
+function extractSignUpInputs(){
+    return {
+        firstName : $('#firstName').val(),
+        lastName : $('#lastName').val(),
+        email : $('#email').val(),
+        phoneNumber : $('#phoneNumber').val(),
+        gender : $('#gender').val(),
+        birthdate :new Date($('#birthDate').val()) ,
+        height : $('#height').val(),
+        weight : $('#weight').val(),
+        password : $('#password').val()
+    }
+}
+function missingInputCheck(user){
+    return Object.keys(user).find(key => user[key] === "" || user[key] == "Invalid Date")
+}
+
+
+function isTrainee() { return $('#type-of-user').val() === "trainee"}
+
+function getPath(){
+    return isTrainee() ? "./user/user.html" : "./trainer/trainer.html"
+}
+function getSignUpFunction(){
+    return isTrainee() ? virtFitApp.creatNewUser : virtFitApp.createNewTrainer
+}
+function getSignInFunction(){
+    return isTrainee() ? virtFitApp.signInUser : virtFitApp.signInTrainer
+}
